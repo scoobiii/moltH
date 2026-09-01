d33c5f838f9cd151d4609c5b5234f836a7ec49b5
root <root@localhost.localdomain>
2026-09-01 11:31:42 +0000

    feat: MExPricing MoltPricing carteira juridica full - 6 wallets R4k - b5b5a48..e8d5af3

--- src/components/molth/MExPricing.tsx
+++ src/components/molth/MExPricing.tsx
@@ -1,4 +1,1 @@
-import React from 'react';
-export default function MExPricing({orgId='mex-427273fd'}:{orgId?:string}){
-  return <div>MEx 6 carteiras R${4}k - org {orgId} - H ROOT 427273fd</div>
-}
+# cola aqui
