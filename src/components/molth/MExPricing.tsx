e8d5af3124b4176a80f18eebcfac42a7be7b0537
root <root@localhost.localdomain>
2026-09-01 11:17:41 +0000

    feat: wallet mesh MEx

---
+++ src/components/molth/MExPricing.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function MExPricing({orgId='mex-427273fd'}:{orgId?:string}){
+  return <div>MEx 6 carteiras R${4}k - org {orgId} - H ROOT 427273fd</div>
+}
